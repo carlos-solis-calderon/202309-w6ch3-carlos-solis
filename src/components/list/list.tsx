60a77539640b6f96c310b0803e9307def6e14746
Carlos Solis <mrsoliscalderon@gmail.com>
2023-11-15 15:47:02 +0100

    fix list component

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -1,9 +1,14 @@
 import { useEffect } from 'react';
 import { Card } from '../card/card';
 import { useCharacters } from '../hook/use.characters';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store/characters.store';
 
 export function List() {
-  const { characters, loadCharacters } = useCharacters();
+  const { characters } = useSelector(
+    (state: RootState) => state.charactersState
+  );
+  const { loadCharacters } = useCharacters();
 
   useEffect(() => {
     loadCharacters();
